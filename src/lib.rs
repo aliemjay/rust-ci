b9f7385ff3cd3c429c0399b14dc5e6b2b3f2f6e5
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2022-04-11 06:42:50 +0300

    TAIT lifetime

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,15 @@
-pub trait Object<'a> { // <- can't remove `'a`
-    type Error; // <- can't remove because then `E`s are no longer constrained below
-    type Future;
+#![feature(generic_associated_types, type_alias_impl_trait)]
+use core::future::Future;
+
+trait Service<'a, Req> {
+    type Future: Future;
+    fn call(req: &'a Req) -> Self::Future;
 }
 
-impl Object<'static> for u8 {
-    type Error = ();
-    type Future = ();
+impl<'a, Req> Service<'a, Req> for u8 {
+    type Future = impl Future;
+    fn call(req: &'a Req) -> Self::Future {
+        async move { let x = req; }
+    }
 }
 
-impl<'a, E, A: Object<'a, Error = E>> Object<'a> for (A,) {
-    type Error = ();
-    type Future = CustomFut<'a, E, A>; // <- Replacing `E` with `A::Error` compiles
-}
-
-pub struct CustomFut<'a, E, A: Object<'a, Error = E>> {
-    ph: std::marker::PhantomData<(A::Future,)>, // <- adding `E` to the tuple compiles
-}
-
-pub trait AsyncFn {
-    fn call_async(&self);
-}
-
-impl<F: Fn() -> Fut, Fut> AsyncFn for F {
-    fn call_async(&self) {}
-}
-
-async fn create<T: Object<'static>>() {
-    let _fut: T::Future = unimplemented!();
-    std::future::ready(()).await; // <- any await point should work
-}
-
-pub fn test() {
-    let _ = create::<(u8,)>.call_async(); // <- .call_async() is important (can't remove or replace with `(...)()`; so is `(u8,)` (`u8` doesn't work)
-}
