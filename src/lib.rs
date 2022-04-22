de5f72786c7c3981e724058c849a07d9bf1f3dfd
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2022-04-23 00:02:47 +0300

    Fn issue

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,32 @@ fn test(f: impl for<'a> Service<&'a u8>) {
     // assert_good(BadCombinator(PhD::<&u8>, f));
     assert_good(BadCombinator(PhD, f));
 }
+mod jac {
+trait Variable<'a> {
+    type Type;
+}
+
+impl Variable<'_> for () {
+    type Type = ();
+}
+
+fn check<F, T>(_: F)
+where
+    F: Fn(T), // <- if removed, all fn_* then require type annotations
+    F: for<'a> Fn(<T as Variable<'a>>::Type),
+    T: for<'a> Variable<'a>,
+{
+}
+
+fn test(arg: impl Fn(())) {
+    fn fn_1(_: ()) {}
+    let fn_2 = |_: ()| ();
+    let fn_3 = |a| fn_1(a);
+    let fn_4 = arg;
+
+    check(fn_1); // Error
+    check(fn_2); // Ok
+    check(fn_3); // Ok
+    check(fn_4); // Error
+}
+}
