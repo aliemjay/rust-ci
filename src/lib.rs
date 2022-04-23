b2b54eab48f005ce5daf5e0049caf84676acb2e1
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2022-04-23 06:33:08 +0300

    project issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,14 @@
-#![feature(generic_associated_types)]
-use std::marker::PhantomData as PhD;
+use std::ops::Deref;
 
-pub trait Universe<'a>: 'static {
-    type Ty;
-}
-
-impl<'a> Universe<'a> for &'static u8 {
-    type Ty = &'a u8;
-}
-
-trait Service<Req> {}
-
-struct BadCombinator<ReqU, S>(PhD<ReqU>, S);
-
-impl<'c, ReqU, S> Service<<ReqU as Universe<'c>>::Ty> for BadCombinator<ReqU, S>
+fn test<T, TDeref>()
 where
-    ReqU: for<'a> Universe<'a>,
-    S: for<'a> Service<<ReqU as Universe<'a>>::Ty>,
-    S: Service<ReqU>,
+    T: Deref<Target = TDeref>,
+    TDeref: ?Sized,
+    for<'a> &'a TDeref: IntoIterator,
+    for<'a> <&'a TDeref as IntoIterator>::IntoIter: Clone,
 {
 }
 
-fn test(f: impl for<'a> Service<&'a u8>) {
-    fn assert_good(_: impl for<'a> Service<&'a u8>) {}
-
-    // Without annotation
-    // assert_good(BadCombinator(PhD::<&u8>, f));
-    assert_good(BadCombinator(PhD::<&u8>, f));
+fn main() {
+    test::<Vec<u8>, _>();
 }
