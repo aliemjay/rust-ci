aa6c795607705f63dc7380fa0830887d7666e778
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2022-05-04 03:06:59 +0300

    GAT inference

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
-#![feature(type_alias_impl_trait)]
-type Ty<'a> = impl Fn() -> &'a str;
-fn defining(s: &str) -> Ty<'_> { move || s }
-fn execute(ty: Ty<'_>) -> &str { ty() }
+#![feature(generic_associated_types)]
+use std::marker::PhantomData;
+
+trait AsyncFn<Arg> { type Output; }
+trait RequestFamily { type Type<'a>; }
+trait Service {}
+
+struct MyFn;
+impl AsyncFn<String> for MyFn { type Output = (); }
+
+impl RequestFamily for String { type Type<'a> = String; }
+
+struct ServiceFromAsyncFn<F, Req>(F, PhantomData<Req>);
+
+impl<F, Req, O, O2> Service for ServiceFromAsyncFn<F, Req>
+where
+    Req: RequestFamily,
+    //F: AsyncFn<Req>,
+    //F: for<'a> AsyncFn<Req::Type<'a>, Output = O>,
+    F: Fn(Req) -> O2,
+    F: for<'a> Fn(Req::Type<'a>) -> O,
+{
+}
+
+fn assert_service() -> impl Service {
+    fn my_fn(_: String) {}
+    ServiceFromAsyncFn(my_fn, PhantomData)
+}
