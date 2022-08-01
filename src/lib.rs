81d0c8b4d63ae6f98ed169b9b6c8fb7dd944849a
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2022-08-01 14:43:32 +0300

    diagnostic regression of inference

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,12 @@
-#![feature(generic_associated_types)]
-use std::marker::PhantomData;
+#![feature(type_alias_impl_trait)]
 
-trait AsyncFn<Arg> { type Output; }
-trait RequestFamily { type Type<'a>; }
-trait Service {}
+type Fut<'a> = impl Sized; // call::<'a, 'empty>::closure#0
 
-struct MyFn;
-impl AsyncFn<String> for MyFn { type Output = (); }
-
-impl RequestFamily for String { type Type<'a> = String; }
-
-struct ServiceFromAsyncFn<F, Req>(F, PhantomData<Req>);
-
-impl<F, Req, O, O2> Service for ServiceFromAsyncFn<F, Req>
+fn call<'a, 'b>(s: &'a str) -> Fut<'a>
 where
-    Req: RequestFamily,
-    //F: AsyncFn<Req>,
-    //F: for<'a> AsyncFn<Req::Type<'a>, Output = O>,
-    F: Fn(Req) -> O2,
-    F: for<'a> Fn(Req::Type<'a>) -> O,
+    'b: 'a,
 {
+    move || { let s: &'a str = s; }
 }
 
-fn assert_service() -> impl Service {
-    fn my_fn(_: String) {}
-    ServiceFromAsyncFn(my_fn, PhantomData)
-}
+fn main() {}
