d585cd8cfdb99d1094be234a53d4fbaa64522c85
Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
2023-02-26 13:05:03 +0300

    TAIT defining use

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,16 @@
-use hyper::http;
+#![feature(type_alias_impl_trait)]
 
-use std::{convert::Infallible, future::Future, net::SocketAddr, sync::Arc};
+use std::future::Future;
 
-async fn request_handler(jvs_file: &str) -> http::Response<hyper::Body> {
-    todo!()
+type FutNothing = impl Future<Output = ()>;
+
+fn indirect() -> FutNothing {
+    call(operation);
+    operation()
 }
 
-async fn service<H, F>(r_handler: Arc<H>) -> Result<http::Response<hyper::Body>, Infallible>
-where
-    H: Fn(&str) -> F + Send,
-    F: Future<Output = http::Response<hyper::Body>> + Send,
-{
-    r_handler(s).await;
-    todo!()
-}
+async fn operation() {}
 
-async fn serve<H, F>(socket: SocketAddr) -> hyper::Result<()>
-where
-    H: Fn(&str) -> F + Send,
-    F: Future<Output = http::Response<hyper::Body>> + Send,
-{
-    let request_handler = Arc::new(request_handler);
-    let service = hyper::service::make_service_fn(|_| {
-        let request_handler = request_handler.clone();
-        async move {
-            Ok::<_, Infallible>(hyper::service::service_fn(move |_| {
-                let request_handler = request_handler.clone();
-                service(request_handler)
-            }))
-        }
-    });
+fn call(_: impl Fn() -> FutNothing) {}
 
-    let server = hyper::server::Server::try_bind(&socket)?;
-    server.serve(service).await?;
-
-    Ok(())
-}
-
-fn main() {
-    println!("Hello, world!");
-}
+fn main() {}
